8543943cae9b9e177672a58239c77cc9688f1c5f
Super3nry23 <enricobiagio@gmail.com>
2024-05-22 22:53:01 +0200

    Start

---
+++ src/components/Card.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { IonButton, IonCard, IonCardContent, IonCardHeader, IonCardTitle } from '@ionic/react';
+
+interface CardProps {
+    title: string;
+    desc: string;
+  }
+
+const Card: React.FC<CardProps> = ({ title="titolo", desc="desc" }) => {
+  return (
+    <IonCard>
+      <IonCardHeader>
+        <IonCardTitle>{title}</IonCardTitle>
+      </IonCardHeader>
+
+      <IonCardContent>{desc}</IonCardContent>
+
+      <IonButton fill="clear">Action 1</IonButton>
+    </IonCard>
+  );
+}
+export default Card;
--- src/pages/Page.tsx
+++ src/pages/Page.tsx
@@ -1,11 +1,39 @@
 import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import { useParams } from 'react-router';
 import ExploreContainer from '../components/ExploreContainer';
+import Card from '../components/Card';
+//API bzz
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+
 import './Page.css';
+
 
 const Page: React.FC = () => {
 
   const { name } = useParams<{ name: string; }>();
+
+  const [patterns, setPatterns] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    axios.get('http://localhost:1337/api/patterns')
+      .then((response) => {
+        
+        const mappedPatterns = response.data.data.map(p => ({
+          title: p.attributes.name,
+          desc: p.attributes.description
+        }));
+        setPatterns(mappedPatterns);
+        //setPatterns(response.data.data);
+        setLoading(true);
+        console.log(patterns);
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  }, []);
 
   return (
     <IonPage>
@@ -25,6 +53,9 @@ const Page: React.FC = () => {
           </IonToolbar>
         </IonHeader>
         <ExploreContainer name={name} />
+        {patterns.map((p) => (
+            <Card title={p.title} desc={p.desc} ></Card>
+          ))}
       </IonContent>
     </IonPage>
   );
